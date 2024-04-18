85d74036b29a520af0c5a2e7142605bb06b51149
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-18 12:59:34 -0300

    feat: adding Projeto_3

---
+++ Projeto_3/main.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Please enter your name: ")
+let lastName = prompt("Please enter your last name: ")
+let age = parseInt(prompt("Please enter your age: "))
+let ageCalc = age + 2;
+
+console.log(`Seu nome é ${firstName} ${lastName}, você tem ${age} anos, Seja bem vindo`)
