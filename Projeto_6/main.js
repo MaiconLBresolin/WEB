313164d7cc110f5039b2d5f5e4205d121cc2cb48
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-18 14:07:44 -0300

    feat: adding Projeto_6

---
+++ Projeto_6/main.js
@@ -0,0 +1,10 @@
+let friends = ['Alan','Mark','Julia','Bella'];
+
+console.log(friends)
+
+/*for (var i = 0; i<6; i++){
+    friends.push('Gabriel') //adição de elementos em array
+}
+*/
+
+console.log(friends.length)
