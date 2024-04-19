41cbfd8e559296cb486eb416d5e92f5d44d244c1
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-19 10:30:28 -0300

    feat: improving Projeto_14

--- Projeto_14/main.js
+++ Projeto_14/main.js
@@ -3,8 +3,8 @@ let corList = [];
 let corCounter = 0;
 for (var i = 0; i<3; i++){
     corCounter = corCounter+1;
-    cor = prompt(`Digite sua cor numero ${corCounter}: `)
+    cor = prompt(`Please enter your color number ${corCounter}: `)
     corList.push(cor)
 }
 
-alert(`Sua lista de cores é ${corList[0]}, ${corList[1]} e ${corList[2]}`) 
+alert(`Your color list is composed by ${corList[0]}, ${corList[1]} e ${corList[2]}`) 
