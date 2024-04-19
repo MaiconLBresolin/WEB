75859d1875576aba4e6511d3f81871cb529f715d
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-19 12:57:21 -0300

    feat: adding Projeto_16

---
+++ Projeto_16/main.js
@@ -0,0 +1,18 @@
+/*function greeting(name){
+    console.log(`Hi ${name}`)
+}
+
+greeting("Maicon")
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+let result = sum(10, 20)
+
+console.log(result)*/
+
+function changeColor(){
+    let titleH1 = document.getElementById('changeH1')
+    titleH1.style.color = 'blue';
+}
