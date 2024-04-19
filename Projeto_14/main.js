49be9465ac72f6b984ba45250f82fe2fbcd02adc
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-19 10:19:38 -0300

    feat: fixing Projeto_14

---
+++ Projeto_14/main.js
@@ -0,0 +1,8 @@
+let cor = ' ';
+let corList = [];
+for (var i = 0; i<3; i++){
+    cor = prompt("Digite sua cor: ")
+    corList.push(cor)
+}
+
+alert(`Sua lista de cores é ${corList[0]}, ${corList[1]} e ${corList[2]}`)
